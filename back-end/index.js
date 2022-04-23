c78c78edd968a489de984890eadb4e99c9985393
Rubens Algeri <rubens_zys@hotmail.com>
2022-04-23 16:32:55 -0300

    adicionar login e tweetar

--- back-end/index.js
+++ back-end/index.js
@@ -2,22 +2,29 @@ import express from "express"
 import cors from "cors"
 import chalk from "chalk"
 
+let infoUsuario = [];
+let tweets = [];
+
 const app = express();
+app.use(express.json())
 app.use(cors());
 
-app.post("/sign-up", (req,res)=>{
-
-    res.send()
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body;
+    infoUsuario.push({username, avatar})
+    res.send("OK")
 })
 
-app.post("/tweets", (req,res)=>{
-
-    res.send()
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body;
+    let tweetUsername = infoUsuario.find(item=>{return item.username == username})
+    tweets.unshift({username, tweet, avatar: tweetUsername.avatar})
+    res.send("OK")
 })
 
-app.get("/tweets", (req,res)=>{
+app.get("/tweets", (req, res) => {
 
-    res.send()
+    res.send(tweets)
 })
 
-app.listen(5000, chalk.bold.green("aplicação no ar"));
+app.listen(5000, console.log(chalk.bold.green("aplicação no ar")));
