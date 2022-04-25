a44729c91e175073852411ecfc0d4f7df582d4f3
Rubens Algeri <rubens_zys@hotmail.com>
2022-04-25 16:49:08 -0300

    adicionar requisição por header, pesquisar tweets por username, paginas de tweets e validar requisições

--- back-end/index.js
+++ back-end/index.js
@@ -11,23 +11,44 @@ app.use(cors());
 
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
-    infoUsuario.push({username, avatar})
-    res.send("OK")
+
+    if (username === '' || avatar === '') {
+        res.status(400).send("Todos os campos são obrigatórios!");
+    } else {
+        infoUsuario.push({ username, avatar })
+        res.status(201).send("OK");
+    }
 })
 
 app.post("/tweets", (req, res) => {
-    const { username, tweet } = req.body;
-    let tweetUsername = infoUsuario.find(item=>{return item.username == username})
-    tweets.unshift({username, tweet, avatar: tweetUsername.avatar})
-    res.send("OK")
+    const { tweet } = req.body;
+    let username  = req.header('user');
+
+    if (username == '' || tweet == '') res.status(400).send("Todos os campos são obrigatórios!");
+    else {
+        let tweetUsername = infoUsuario.find(item => { return item.username == username })
+        tweets.unshift({ username, tweet, avatar: tweetUsername.avatar })
+        res.status(201).send("OK");
+    }
 })
 
 app.get("/tweets", (req, res) => {
-    if(tweets.length>10){
-        tweets = tweets.slice(0,10)
-    }
+    const page = req.query.page;
+    if (page >= 1) {
+        if (tweets.length > 10) {
+        }
 
-    res.send(tweets)
+        res.status(200).send(tweets.slice((page-1)*10, (page)*10))
+    } else res.status(400).send("Informe uma página válida!")
+})
+
+app.get("/tweets/:username", (req, res) => {
+    const {username} = req.params;
+    console.log(username)
+    let userTweets = tweets.filter(item => { return item.username == username })
+    if (userTweets) {
+        res.status(200).send(userTweets)
+    }else res.status(400).send("Informe um usuário válido!")
 })
 
 app.listen(5000, console.log(chalk.bold.green("aplicação no ar")));
